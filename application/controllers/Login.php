b172a68cb38270372aaf20a86144290996af7502
eduardovk <eduardo_vk@hotmail.com>
2020-05-04 18:31:12 -0300

    Controller Login refatorando utilizando helpers

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,28 +3,23 @@
 class Login extends CI_Controller{
 
     public function index(){
-
-        $data['title'] = "Fazer Login";
-
-        $data['erro'] = $this->session->flashdata('erro');
-        $data['sucesso'] = $this->session->flashdata('sucesso');
-        $this->load->view('templates/msg_erro', $data);
-        $this->load->view('templates/msg_sucesso', $data);
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('login/index', $data);
-        $this->load->view('templates/footer');
+        $data = array();
+        //capturar nome do usuario e nivel de acesso (caso logado)
+        identificar_usuario($this, $data);
+        //carrega header, msgs de erro, login/index e footer
+        carregar_views($this, 'login/index', $data);
     }
+
 
     public function entrar(){
         $this->load->model('usuarios_model');
-
+        //captura email e senha informados
         $email = $this->input->post("email");
         $senha = sha1($this->input->post("senha"));
-
         $usuario = $this->usuarios_model->get_usuario($email, $senha);
-
+        //se foi encontrado usuario com email e senha no bd
         if($usuario){
+            //inicializa variaveis de sessao
             $this->session->set_userdata("nome", $usuario["nome"]);
             $this->session->set_userdata("admin", $usuario["admin"]);
             $this->session->set_userdata("id_usuario", $usuario["id"]);
@@ -35,34 +30,28 @@ class Login extends CI_Controller{
         }
     }
 
+
     public function cadastrar(){
         $this->load->model('usuarios_model');
         $this->load->helper('form');
         $this->load->library('form_validation');
-
+        $data = array();
         $this->form_validation->set_rules('email','E-mail','trim|required');
         $this->form_validation->set_rules('nome','Nome Completo','trim|required');
         $this->form_validation->set_rules('senha', 'Senha', 'required|min_length[3]');
         $this->form_validation->set_rules('senha2', 'Repita a senha', 'required|matches[senha]');
 
-        if ($this->form_validation->run() === FALSE)
-        {
-            $data['erro'] =  validation_errors();
-            $this->load->view('templates/header');
-            $this->load->view('templates/msg_erro', $data);
-            $this->load->view('templates/msg_sucesso', $data);
-            $this->load->view('login/cadastro', $data);
-            $this->load->view('templates/footer');
-        }
-        else
-        {
+        if ($this->form_validation->run() === FALSE){
+            $this->session->set_flashdata('erro', validation_errors());
+            //carrega header, msgs de erro, login/cadastro e footer
+            carregar_views($this, 'login/cadastro', $data);
+        }else{
+            //verifica se ja existe usuario com mesmo email cadastrado
             $usuario = $this->usuarios_model->get_usuario_email($this->input->post('email'));
             if($usuario){
-                $data['erro'] = 'Já existe uma uma conta utilizando o email informado!';
-                $this->load->view('templates/header');
-                $this->load->view('templates/msg_erro', $data);
-                $this->load->view('login/cadastro', $data);
-                $this->load->view('templates/footer');
+                $this->session->set_flashdata('erro', 'Já existe uma uma conta utilizando o email informado!');
+                //carrega header, msgs de erro, login/cadastro e footer
+                carregar_views($this, 'login/cadastro', $data);
             }else{
                 if($this->usuarios_model->set_usuario()){
                     $this->session->set_flashdata('sucesso', 'Cadastro concluído com sucesso! Utilize seu e-mail e senha recém cadastrados para fazer o Login:');
@@ -70,8 +59,8 @@ class Login extends CI_Controller{
                 }
             }
         }
-
     }
+
 
     public function logout(){
         if($this->acesso->logged_user()){
@@ -82,5 +71,6 @@ class Login extends CI_Controller{
         }
         redirect("login");
     }
+
 
 }
