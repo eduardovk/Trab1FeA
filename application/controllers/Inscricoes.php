4e057a28f77a468388931afca650e45433ba3d1e
eduardovk <eduardo_vk@hotmail.com>
2020-05-04 18:19:00 -0300

    Controller Inscricoes refatorado utilizando helpers

--- application/controllers/Inscricoes.php
+++ application/controllers/Inscricoes.php
@@ -1,23 +1,21 @@
 <?php
 class Inscricoes extends CI_Controller {
 
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->load->model('inscricoes_model');
         $this->load->helper('url_helper');
     }
 
-    public function index()
-    {
+    
+    public function index(){
+        //impede acesso se nao for usuario logado
         if(!$this->acesso->logged_user()){
             redirect('login');
         }
-
         $id_usuario = $this->session->userdata("id_usuario");
-
         $data['inscricoes'] = $this->inscricoes_model->get_inscricoes_by_usuario($id_usuario);
-
+        //para cada inscricao, formata valor e registro de pago
         foreach($data['inscricoes'] as &$inscricao){
             $inscricao['valor'] = formatar_moeda($inscricao['valor'], true);
             if($inscricao['pago'] == 1){
@@ -26,31 +24,23 @@ class Inscricoes extends CI_Controller {
                 $inscricao['pago'] = "Não";
             }
         }
-
-        $data['nome'] = $this->acesso->logged_user();
-        $data['admin'] = $this->acesso->is_admin();
-
-        $this->load->view('templates/header', $data);
-
-        $data['erro'] = $this->session->flashdata('erro');
-        $data['sucesso'] = $this->session->flashdata('sucesso');
-        $this->load->view('templates/msg_erro', $data);
-        $this->load->view('templates/msg_sucesso', $data);
-
-        $this->load->view('inscricoes/index', $data);
-        $this->load->view('templates/footer');
+        //capturar nome do usuario e nivel de acesso (caso logado)
+        identificar_usuario($this, $data);
+        //carrega header, msgs de erro, inscricoes/index e footer
+        carregar_views($this, 'inscricoes/index', $data);
     }
+
 
     public function inscricoes_evento($id_evento){
         $this->load->model('eventos_model');
-
+        //impede acesso se nao for admin
         $this->acesso->controlar();
-
         $data['inscricoes'] = $this->inscricoes_model->get_inscricoes_by_evento($id_evento);
         $data['evento'] = $this->eventos_model->get_evento_by_id($id_evento);
+        //formata data e hora antes de exibir
         $data['evento']['data'] = formatar_data($data['evento']['data_hora'], true);
         $data['evento']['hora'] = formatar_hora($data['evento']['data_hora'], false);
-
+        //para cada inscricao, formata valor e registro de pago
         foreach($data['inscricoes'] as &$inscricao){
             $inscricao['valor'] = formatar_moeda($inscricao['valor'], true);
             if($inscricao['pago'] == 1){
@@ -59,21 +49,11 @@ class Inscricoes extends CI_Controller {
                 $inscricao['pago'] = "Não";
             }
         }
-
-        $data['nome'] = $this->acesso->logged_user();
-        $data['admin'] = $this->acesso->is_admin();
-
-        $this->load->view('templates/header', $data);
-
-        $data['erro'] = $this->session->flashdata('erro');
-        $data['sucesso'] = $this->session->flashdata('sucesso');
-        $this->load->view('templates/msg_erro', $data);
-        $this->load->view('templates/msg_sucesso', $data);
-
-        $this->load->view('inscricoes/inscricoes_evento', $data);
-        $this->load->view('templates/footer');
+        //capturar nome do usuario e nivel de acesso (caso logado)
+        identificar_usuario($this, $data);
+        //carrega header, msgs de erro, inscricoes/inscricoes_evento e footer
+        carregar_views($this, 'inscricoes/inscricoes_evento', $data);
     }
-
 
 
 }
